50bd60a6249e3ad90cd913de5b45ed16b6fab19d
Paul Scanlon <pauliescanlon@gmail.com>
2018-06-05 17:08:14 +0100

    stuff

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const Index = () => {
+	return <div>Welcome to next.js!</div>;
+};
+
+export default Index;
+
+// interface IProps {
+// 	thing?: any;
+// }
+
+// const Index: React.SFC<IProps> = ({ thing: any }) => {
+// 	return <div>Welcome to next.js!</div>;
+// };
+
+// export default Index;
