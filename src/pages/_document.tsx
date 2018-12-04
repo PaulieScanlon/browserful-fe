b4100a34d815419b87a023abe27364f7d9cc0cf4
Paul Scanlon <pauliescanlon@gmail.com>
2018-12-04 15:29:20 +0000

    adding static folder

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,18 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/images/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/images/favicon-16x16.png"
+            sizes="16x16"
+          />
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
           <link
             href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
