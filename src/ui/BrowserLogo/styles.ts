9dc19e4d71df5dc336c9dc7be61cc49549ec4854
Paul Scanlon <pauliescanlon@gmail.com>
2019-01-13 11:02:08 +0000

    hi res browser logos and style amends to chart

--- src/ui/BrowserLogo/styles.ts
+++ src/ui/BrowserLogo/styles.ts
@@ -3,6 +3,8 @@ import { scaffolding } from '../../theme';
 
 export const BrowserImage = styled.img({
   label: 'browser-image',
+  width: '38px',
+  height: 'auto',
   margin: `${scaffolding.gutterSm} ${scaffolding.gutterSm} ${
     scaffolding.gutterSm
   } 0px`
