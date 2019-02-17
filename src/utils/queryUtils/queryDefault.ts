ff2d4fe762e2e109a3218daaefc4e3e799238433
Paul Scanlon <pauliescanlon@gmail.com>
2019-02-17 15:08:52 -0800

    updating settings and standard excluded

--- src/utils/queryUtils/queryDefault.ts
+++ src/utils/queryUtils/queryDefault.ts
@@ -1,14 +1,12 @@
 import { config } from '../../features/MatrixUi/config/sliderControls.config';
 import { config as uiMisc } from '../../features/MatrixUi/config/uiMisc.config';
 import { queryParams } from '../enums';
-// import { standardExcludedQuery } from './standardExcluded';
-
-// &gu=${config[queryParams.GLOBAL_USAGE].slider.defaultValue}&yr=${
-//   config[queryParams.YEAR_RELEASED].slider.defaultValue
-// }&excq=${standardExcludedQuery}
+import { standardExcludedQuery } from './standardExcluded';
 
 export const queryDefault = {
   DEFAULT_QUERY: `?mn=${uiMisc[queryParams.MATRIX_NAME].name}&lv=${
     config[queryParams.LAST_VERSIONS].slider.defaultValue
-  }`
+  }&gu=${config[queryParams.GLOBAL_USAGE].slider.defaultValue}&yr=${
+    config[queryParams.YEAR_RELEASED].slider.defaultValue
+  }&incq=&excq=${standardExcludedQuery}`
 };
